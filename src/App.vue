6040e5eae869cbfaf404802a6635f0b120a35cf4
smboolean76 <smboolean76@madrigali.dev>
2022-11-30 11:05:25 +0100

    recap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <script>
+import { store } from "./store";
+import axios from "axios";
+
 import HelloWorld from "./components/HelloWorld.vue";
 export default {
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    saluta(data) {
+      console.log(data);
+    },
+  },
+  created() {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      this.store.pokemon = res.data.results;
+    });
+
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: "e99307154c6dfb0b4750f6603256716d",
+          query: "ritorno",
+          language: "it-IT",
+        },
+      })
+      .then((res) => {
+        this.store.movies = res.data.results;
+      });
   },
 };
 </script>
 
 <template>
   <div>
-    <HelloWorld />
+    <h2 style="color: red" v-if="store.text === 'franco'">{{ store.text }}</h2>
+    <HelloWorld @cliccato="saluta" msg="Hello World" />
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,32 @@
 <script>
+import { store } from "../store";
+
 export default {
   name: "HelloWord",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
 };
 </script>
 
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1>{{ msg }}</h1>
+    <button @click="$emit('cliccato', 'Buongiorno')">Cliccami</button>
+    <select v-model="store.text">
+      <option value="">Seleziona un nome</option>
+      <option value="franco">Franco</option>
+      <option value="antonio">Antonio</option>
+    </select>
+
+    <ul>
+      <li v-for="poke in store.pokemon">{{ poke.name }}</li>
+    </ul>
   </div>
 </template>
 
